92952b08af73f1a1d859054a5150661e2c4ec3ff
Derek Neuts <dcneuts@gmail.com>
2017-07-13 01:01:16 -0700

    Cosmetic modification and unversioned file from PHPStorm

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var http = require('http');
 http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
+	// \n is the return character
 	response.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running on port 1337');

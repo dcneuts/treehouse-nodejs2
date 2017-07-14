30b20b01312e35c25280aa7a87d3b02ec02c29d4
Derek Neuts <dcneuts@gmail.com>
2017-07-13 23:46:21 -0700

    Setting up the GET method for Treehouse data, defining specific JSON object variables, had to call length method on badges due to very large arrays, setting up error handling

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 /**
  * Created by Derek on 7/13/2017.
  */
+var Profile = require("./profile.js");
+
 //HTTP route GET and POST
 function home(request, response) {
 	if(request.url === "/") {
@@ -17,6 +19,29 @@ function user(request, response) {
 	if(username.length > 0) {
 		response.writeHead(200, {'Content-Type': 'text/plain'});
 		response.write("Header\n");
+
+		//JSON from Treehouse
+		var studentProfile = new Profile(username);
+
+		//on end
+		studentProfile.on("end", function (profileJSON) {
+			//show profile
+
+			//store values from JSON object
+			var values = {
+				avatarUrl: profileJSON.gravatar_url,
+				username: profileJSON.profile_name,
+				badges: profileJSON.badges.length,
+				javascriptPoints: profileJSON.points.JavaScript
+			}
+			//simple response
+
+		});
+		//error handler
+		studentProfile.on("error", function (error) {
+			//show the error
+
+		});
 		response.write(username + "\n");
 		response.end('Footer\n');
 	}

28eb72183e43f61299e9bdaca38d8c0c2ddffa05
Derek Neuts <dcneuts@gmail.com>
2017-07-13 22:18:20 -0700

    Added handler for pulling username

--- app.js
+++ app.js
@@ -16,3 +16,13 @@ function homeRoute(request, response) {
 	}
 
 }
+//Handle GET username
+function userRoute(request, response) {
+	var username = request.url.replace("/", "");
+	if(username.length > 0) {
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write("Header\n");
+		response.write(username + "\n");
+		response.end('Footer\n');
+	}
+}
